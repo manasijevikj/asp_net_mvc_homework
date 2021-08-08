5ea2b14016d2ccdf0ed4f3a5531e238abf609481
manasijevikj <aleksandar.manasijevikj@gmail.com>
2021-08-08 21:16:39 +0200

    Pizza App

---
+++ Homework_Class02/SEDC.PizzaApp/SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Controllers
+{
+
+    public class OrderController : Controller
+    {
+        [Route("Orders")]
+        public IActionResult Index()
+        {
+            List<Order> orders = StaticDb.Orders;
+            return View(orders); 
+        }
+         
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new EmptyResult();
+            }
+            Order order = StaticDb.Orders.FirstOrDefault(x => x.Id == id);
+            if (order == null)
+            {
+                return new EmptyResult();
+            }
+            return View(order);
+        }
+
+        public IActionResult JsonData()
+        {
+            Order order = new Order()
+            {
+                Id = 3,
+                Customer = "Zafir"
+            };
+            return new JsonResult(order);
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
---
+++ Homework_Class02/SEDC.PizzaApp/SEDC.PizzaApp/Models/Domain/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Models.Domain
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public TimeSpan Date { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+        public string Customer { get; set; }
+
+    }
+}
+ 
