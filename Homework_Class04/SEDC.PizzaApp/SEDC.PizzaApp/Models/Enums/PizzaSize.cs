c4cdf5d6b1355d3adb8535e40e621d05d8122e7c
manasijevikj <aleksandar.manasijevikj@gmail.com>
2021-08-12 15:34:44 +0200

    all 3

---
+++ Homework_Class04/SEDC.PizzaApp/SEDC.PizzaApp/Models/Enums/PizzaSize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Models.Enums
+{
+    public enum PizzaSize
+    {
+        Normal = 1,
+        Family
+    }
+}
---
+++ Homework_Class04/SEDC.PizzaApp/SEDC.PizzaApp/Models/Mappers/UserMapper.cs
@@ -0,0 +1,20 @@
+﻿using SEDC.PizzaApp.Models.Domain;
+using SEDC.PizzaApp.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Models.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserViewModel UserToUserViewModelExtensionMethod(this User user)
+        {
+            return new UserViewModel
+            {
+                FullName = $"{user.FirstName} {user.LastName}"
+            };
+        }
+    }
+}
